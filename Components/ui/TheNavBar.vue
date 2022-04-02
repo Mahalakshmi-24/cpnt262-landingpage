5b8adcf5119e14d88a782838dc91ade01379b4a5
Mahalakshmi <mahait07@gmail.com>
2022-04-02 08:51:51 -0600

    navbar added

--- Components/ui/TheNavBar.vue
+++ Components/ui/TheNavBar.vue
@@ -1,9 +1,10 @@
 <template>
  
-  <nav class="flex gap-10 m-auto justify-items-end text-xl font-extrabold">
-    <a :href="'/'" class="hover:text-red-400">Home</a>
-    <a :href="'/'" class="hover:text-red-400">About</a>
-    <a :href="'/'" class="hover:text-red-400">Contact</a>
+  <nav class="flex gap-10 m-auto justify-items-end   text-xl italic bg-black p-8">
+    <a :href="'/'" class="text-white ">Home</a>
+    <a :href="'/'" class="text-white">About</a>
+    <a :href="'/'" class="text-white">Gallery</a>
+    <a :href="'/'" class="text-white">Contact</a>
   </nav>
 
 </template>
