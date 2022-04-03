8439194c3bb566fe161fcf96a2660aef142f5562
Mahalakshmi <mahait07@gmail.com>
2022-04-03 12:22:56 -0600

    made some changes in nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: [],
+  css: ['@/assets/css/tailwind.css'],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   components: [
     "~/Components",
     "~/Components/ui",
